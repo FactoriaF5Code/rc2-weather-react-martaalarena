3fcd413c33589cc3a802fa8055b443495eaf7714
Marta <martalarena2002@gmail.com>
2024-01-13 16:31:13 +0100

    skeleton done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css';
-
+import "./App.css";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function App() {
-return (
-<>
-<h1>Predicción del tiempo</h1>
-<input type="text" placeholder='El tiempo en ...'/>
-<button><Search/></button>
-</>
-)
+  return (
+    <>
+      <div className="container">
+        <h1 className="titulo">Prediccion del tiempo</h1>
+        <section className="contenedor_busqueda">
+          <input className="input" type="text" placeholder="El tiempo en ..." />
+          <button className="lupa">
+            <SearchIcon />
+          </button>
+        </section>
+      </div>
+    </>
+  );
 }
-
