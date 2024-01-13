99f69f63fb5df3244319267fbf612c7c4c4674dd
Marta <martalarena2002@gmail.com>
2024-01-13 17:41:05 +0100

    api

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,12 +1,20 @@
-
+import { useEffect } from "react";
 
 export default function Api() {
-    const Apikey = "a259fc7cb99fb6a5662bfe50c2ddeef0"
-    const APIcall= () => {
-        
-    }
-   
-  return (
-    <div>Api</div>
-  )
+    const [data, setData] = useState()
+
+  const Apikey = "a259fc7cb99fb6a5662bfe50c2ddeef0";
+
+  useEffect(() => {
+    fetch(
+        "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid= " + Apikey
+    );
+
+    .then((reponse) => response.json())
+    .then((data) => {
+        setData(Array.isArray(data.))
+    })
+  });
+
+  return <div>Api</div>;
 }
